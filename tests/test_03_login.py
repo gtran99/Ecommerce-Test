feac05ede225a52f596be9ca6a058b7e7ff0e02d
gtran99 <gavintran1@gmail.com>
2024-06-05 20:55:43 -0700

    Verify logout in login test case too

--- tests/test_03_login.py
+++ tests/test_03_login.py
@@ -1,11 +1,21 @@
 from selenium.webdriver.common.by import By
 
-def test_login(driver):
+def test_logout(driver):
+    # Log in first
     driver.get("http://127.0.0.1:5000/login")
     driver.find_element(By.NAME, "email").send_keys("testuser@example.com")
     driver.find_element(By.NAME, "password").send_keys("password")
     driver.find_element(By.NAME, "submit").click()
 
-    # Verify login success
+
+    # Verify login
     success_message = driver.find_element(By.CLASS_NAME, "alert-success").text
     assert "Logged in successfully!" in success_message
+
+    # Log out
+    driver.find_element(By.LINK_TEXT, "Logout").click()
+
+    # Verify logout
+    assert "Login" in driver.page_source
+    assert "Register" in driver.page_source
+    assert "Logout" not in driver.page_source
