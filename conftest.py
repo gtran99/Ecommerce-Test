f99e6d49b52145204f741be14ec468921bdd0bd5
gtran99 <gavintran1@gmail.com>
2024-06-08 19:39:03 -0700

    Integrating Webdriver_manager for driver
    Using this to deal with ChromeDriver version mismatching due to most recent Linux link being older

--- conftest.py
+++ conftest.py
@@ -1,18 +1,10 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-import os
+from webdriver_manager.chrome import ChromeDriverManager
 
 @pytest.fixture(scope="module")
 def driver():
-    # Determine the path to the ChromeDriver based on the environment
-    if os.name == 'nt':  # Windows
-        driver_path = os.path.join('drivers', 'chromedriver.exe')
-    else:  # macOS or Linux for CI
-        driver_path = os.path.join('drivers', 'chromedriver')
-
-    # Initialize the WebDriver
-    driver = webdriver.Chrome(executable_path=driver_path)
-    driver.implicitly_wait(10)  # Wait up to 10 seconds for elements to appear
+    # Initialize the WebDriver with WebDriverManager
+    driver = webdriver.Chrome(ChromeDriverManager().install())
     yield driver
     driver.quit()
