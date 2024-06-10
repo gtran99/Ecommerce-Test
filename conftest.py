554beebdaa77690cbcee2eba35aabfe7d6c0f040
gtran99 <gavintran1@gmail.com>
2024-06-10 16:01:43 -0700

    Test CI without headless 2

--- conftest.py
+++ conftest.py
@@ -6,11 +6,12 @@ from webdriver_manager.chrome import ChromeDriverManager
 @pytest.fixture(scope="session")
 def driver():
     service = ChromeService(executable_path=ChromeDriverManager().install())
-    options = webdriver.ChromeOptions()
+    driver = webdriver.Chrome(service=service)
+    # options = webdriver.ChromeOptions()
     # options.add_argument('--headless')  # Run in headless mode for CI
     # options.add_argument('--no-sandbox')
     # options.add_argument('--disable-dev-shm-usage')
-    driver = webdriver.Chrome(service=service, options=options)
+    # driver = webdriver.Chrome(service=service, options=options)
 
      # Maximize the browser window (important for login tests)
     driver.maximize_window()
