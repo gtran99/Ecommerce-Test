64fd367d92dad529f7e959d9c23e1eb6038ce09e
gtran99 <gavintran1@gmail.com>
2024-06-09 20:50:41 -0700

    Start Flask in the background

--- run.py
+++ run.py
@@ -1,4 +1,9 @@
 from app import app
+import os
+
+# Set default host and port
+host = os.getenv('FLASK_RUN_HOST', '127.0.0.1')
+port = os.getenv('FLASK_RUN_PORT', 5000)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host=host, port=port, debug=True)
