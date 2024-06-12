cadf4aa3b63fa250e3d5dcefabd93c9532b5cf42
gtran99 <gavintran1@gmail.com>
2024-06-12 13:32:18 -0700

    CI and Conftest changes
    CI:
    - Update CI filepath to check updated ChromeDriver version
    
    Conftest
    - Use specific filepath rather than ChromeDriverManager

--- conftest.py
+++ conftest.py
@@ -1,13 +1,12 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
 
 @pytest.fixture(scope="session")
 def driver():
-    service = ChromeService(executable_path=ChromeDriverManager().install())
+    service = ChromeService(executable_path="./drivers/chromedriver")
     options = webdriver.ChromeOptions()
-    # options.add_argument('--headless')  # Run in headless mode for CI
+    # options.add_argument('--headless')  # Uncomment for headless mode in CI
     # options.add_argument('--no-sandbox')
     # options.add_argument('--disable-dev-shm-usage')
     options.add_argument("--start-maximized")  # Start the browser maximized (important for login tests)
