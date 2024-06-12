4a0fe377efc071bb7e1257cec162b121abca3cf3
gtran99 <gavintran1@gmail.com>
2024-06-11 17:32:45 -0700

    CI Cleanup and updating Conftest

--- conftest.py
+++ conftest.py
@@ -6,14 +6,13 @@ from webdriver_manager.chrome import ChromeDriverManager
 @pytest.fixture(scope="session")
 def driver():
     service = ChromeService(executable_path=ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service)
-    # options = webdriver.ChromeOptions()
+    options = webdriver.ChromeOptions()
     # options.add_argument('--headless')  # Run in headless mode for CI
     # options.add_argument('--no-sandbox')
     # options.add_argument('--disable-dev-shm-usage')
-    # driver = webdriver.Chrome(service=service, options=options)
-
-     # Maximize the browser window (important for login tests)
+    options.add_argument("--start-maximized")  # Start the browser maximized (important for login tests)
+    
+    driver = webdriver.Chrome(service=service, options=options)
     driver.maximize_window()
     yield driver
     driver.quit()
