1d413ca236b63c22d46a0d06d2e470860459b389
gtran99 <gavintran1@gmail.com>
2024-06-11 19:02:26 -0700

    Reverting to previous commit
    Using updated Chromedriver link for script to fetch correct version

--- conftest.py
+++ conftest.py
@@ -1,12 +1,11 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
 
 @pytest.fixture(scope="session")
-def driver(request):
-     # Use the ChromeDriver from the drivers folder
-    chrome_driver_path = request.config.getoption("--chromedriver-executable-path")
-    service = ChromeService(executable_path=chrome_driver_path)
+def driver():
+    service = ChromeService(executable_path=ChromeDriverManager().install())
     options = webdriver.ChromeOptions()
     # options.add_argument('--headless')  # Run in headless mode for CI
     # options.add_argument('--no-sandbox')
@@ -16,6 +15,3 @@ def driver(request):
     driver = webdriver.Chrome(service=service, options=options)
     yield driver
     driver.quit()
-
-def pytest_addoption(parser):
-    parser.addoption("--chromedriver-executable-path", action="store", default="./drivers/chromedriver.exe")
