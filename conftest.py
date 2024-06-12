1282170f4e34963faac0e6c0db3954f4a2f44cf7
gtran99 <gavintran1@gmail.com>
2024-06-12 13:53:36 -0700

    Testing CI with headless mode enabled

--- conftest.py
+++ conftest.py
@@ -4,13 +4,17 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 
 @pytest.fixture(scope="session")
 def driver():
-    service = ChromeService(executable_path="./drivers/chromedriver")
+    service = ChromeService(executable_path="./drivers/chromedriver") # Append .exe to chromedriver if testing locally
     options = webdriver.ChromeOptions()
-    # options.add_argument('--headless')  # Uncomment for headless mode in CI
-    # options.add_argument('--no-sandbox')
-    # options.add_argument('--disable-dev-shm-usage')
-    options.add_argument("--start-maximized")  # Start the browser maximized (important for login tests)
-    
+    options.add_argument('--headless')  # Run in headless mode for CI
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage') # Prevent issues due to limited shared memory
+    options.add_argument('--disable-gpu') # Disable GPU hardware acceleration
+    options.add_argument('--remote-debugging-port=9222') # Enable remote debugging
+    options.add_argument("--window-size=1920,1080")  # Ensure sufficient size for UI elements
+    options.add_argument('--disable-extensions') 
+    options.add_argument('--disable-software-rasterizer')
+
     driver = webdriver.Chrome(service=service, options=options)
     yield driver
     driver.quit()
